620a627ada137cf6221469ce00b869cc106659c4
Your Name <you@example.com>
2024-01-31 14:27:08 +0900

    params처리

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def main():
-    return {"message": "Hello World"}
+@app.get('/', response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse(request=request,name="index.html" , context={"name":"홍정민"})
+
+
+@app.get('/hello/{name}', response_class=HTMLResponse)
+async def hello(request: Request, name):
+    words = f'당신의 이름은 {name} 이니까 반가워서 인사'
+    return templates.TemplateResponse(request=request,name="hello.html" , context={"name":words})
+
